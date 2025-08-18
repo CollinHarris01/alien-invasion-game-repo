34fd9fc5d77a04594e069af62da62bab19c9dcbe
Collin Harris <collinharris595@gmail.com>
2025-08-18 17:43:07 -0400

    Add settings file and modify main file accordingly

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
@@ -9,19 +11,20 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
 
-        # Set the game clock.
+        # Initialize game clock.
         self.clock = pygame.time.Clock()
 
-        # Set the display screen.
-        self.screen = pygame.display.set_mode((900, 600))
-        pygame.display.set_caption("Alien Invasion")
+        # Initialize game settings.
+        self.settings = Settings()
 
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
+        # Set up game display window.
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
     
     def run_game(self):
         """Start the main loop for the game."""
-        
         while True:
             # Watch for keyboard and mouse events.
             for event in pygame.event.get():
@@ -29,7 +32,7 @@ class AlienInvasion:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             
             # Make the most recently drawn screen visible.
             pygame.display.flip()
