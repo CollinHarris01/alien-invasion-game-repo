a04ea90e05f0ed24d2155981e45af6d400df28cc
Collin Harris <collinharris595@gmail.com>
2025-08-22 15:42:57 -0400

    Refactor alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -50,12 +50,13 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif event.key == pygame.K_q:
-            sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
+        """Respond to key releases."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
